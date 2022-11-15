1b708f742f0bfd5a75f996ced56232a4ca7cdd19
Vinson Wong <vinson.wong93@gmail.com>
2022-11-15 01:58:36 -0500

    complete exercise 5

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -11,14 +11,14 @@ store4 = Store.create(name: "Surrey", annual_revenue: 224000, mens_apparel: fals
 store5 = Store.create(name: "Whistler", annual_revenue: 1900000, mens_apparel: true, womens_apparel: false)
 store6 = Store.create(name: "Yaletown", annual_revenue: 430000, mens_apparel: true, womens_apparel: true)
 
-@mens_stores = Store.where(mens_apparel: true)
+mens_stores = Store.where(mens_apparel: true)
 
-@mens_stores.each do |element|
+mens_stores.each do |element|
   puts "Name: #{element.name} Annual Revenue: #{element.annual_revenue}"
 end
 
-@womens_stores = Store.where(womens_apparel: true)
+womens_stores = Store.where(womens_apparel: true).where("annual_revenue < ?", 1000000)
 
-@womens_stores.each do |element|
+womens_stores.each do |element|
   puts "Name: #{element.name} Annual Revenue: #{element.annual_revenue}"
 end
--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,12 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+total_revenue = Store.sum(:annual_revenue)
+average_revenue = Store.average(:annual_revenue)
+
+puts "Total Revenue for all stores: #{total_revenue}"
+puts "Average Revenue for all stores: #{average_revenue}"
+
+million_revenue = Store.where("annual_revenue > ?", [1000000]).count
+puts "The number of stores with over $1,000,000 in revenue are: #{million_revenue}."
+
