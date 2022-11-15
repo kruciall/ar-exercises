55dea9468c5f67e2389130b795d27c91a9704c73
Vinson Wong <vinson.wong93@gmail.com>
2022-11-15 01:04:08 -0500

    complete exercise 2

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,16 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+
+store1 = Store.find_by(id: 1)
+p store1
+
+
+store1.name = "Toronto"
+store2 = Store.find_by(id: 2)
+
+p store1
+
+
+
+p store2
